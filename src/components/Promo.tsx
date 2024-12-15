be813aa2d578175a33d1c4233a7012b7b5329243
loststargames <cdmarketgame@gmail.com>
2024-12-15 18:21:00 -0300

    fix promo links

--- src/components/Promo.tsx
+++ src/components/Promo.tsx
@@ -21,18 +21,34 @@ export const Promo: React.FC = () => {
               A music label simulation game set in the 90s for iOS and Android.
             </AlertDescription>
             <AlertDescription>
-              <a href="https://x.com/cdmarketgame" target="_blank">
+              <a
+                href="https://x.com/cdmarketgame"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 https://x.com/cdmarketgame
               </a>
             </AlertDescription>
           </div>
           <div className="flex flex-col sm:flex-row sm:space-x-2 space-y-2 sm:space-y-0 ml-4 mt-2 sm:mt-0">
-            <Button variant="outline" className="w-full sm:w-auto">
-              <Apple className="mr-1" /> iOS
-            </Button>
-            <Button variant="outline" className="w-full sm:w-auto">
-              <Play className="mr-1" /> Android
-            </Button>
+            <a
+              href="https://apps.apple.com/us/app/cd-market/id6670562960"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button variant="outline" className="w-full sm:w-auto">
+                <Apple className="mr-1" /> iOS
+              </Button>
+            </a>
+            <a
+              href="https://play.google.com/store/apps/details?id=com.loststargames.cdmarket.android"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button variant="outline" className="w-full sm:w-auto">
+                <Play className="mr-1" /> Android
+              </Button>
+            </a>
           </div>
         </div>
       </Alert>
