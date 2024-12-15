9d494f44f5c8f059f112a9277976f32f7d80cfbb
loststargames <cdmarketgame@gmail.com>
2024-12-15 17:40:29 -0300

    add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/the-tycoon-list/",
   plugins: [react()],
   resolve: {
     alias: {
